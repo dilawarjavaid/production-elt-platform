754c500395c4aa1b2f97f6a46c892fdd6f6a3aac
Dilawar Javaid <dilawar.javaid864@gmail.com>
2026-09-02 22:16:12 +0500

    Configure S3 versioning and public access protection

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,3 +22,20 @@ output "raw_bucket_name" {
 output "raw_bucket_region" {
   value = aws_s3_bucket.raw_bucket.region
 }
+
+resource "aws_s3_bucket_versioning" "raw_bucket_versioning" {
+  bucket = aws_s3_bucket.raw_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "raw_bucket_public_access" {
+  bucket = aws_s3_bucket.raw_bucket.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
